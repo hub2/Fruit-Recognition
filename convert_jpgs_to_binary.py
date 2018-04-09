dd7d693423bee0eb823adb8529b0bc9094739228
Hubert Barc <hubert2222@gmail.com>
2018-04-09 23:53:06 +0200

    bug fix: order of labels was filesystem dependant

--- convert_jpgs_to_binary.py
+++ convert_jpgs_to_binary.py
@@ -3,38 +3,42 @@ import os
 import numpy as np
 import random
 import struct
+
+def image_to_labeled_bytearray(path, label):
+    i = Image.open(path)
+    i.thumbnail((25,25), Image.ANTIALIAS)
+    a = np.array(i)
+    rs = bytearray(a[:,:,0])
+    gs = bytearray(a[:,:,1])
+    bs = bytearray(a[:,:,2])
+    return bytearray([label]) + rs + gs + bs
+
 def convert(path):
     files = []
     classes = {}
     i = 0
-    for dirname in os.listdir(path):
+    print(os.listdir(path))
+
+    for dirname in sorted(os.listdir(path)):
         classes[dirname] = i
         i+=1
         for sample in os.listdir(os.path.join(path, dirname)):
             files.append((path, dirname, sample))
-    #classes = {"Apple Golden 3": 0, "Banana": 1, "Cherry": 2, "Cocos": 3, "Mango": 4, "Orange": 5, "Pineapple": 6}
+
 
     random.shuffle(files)
     out_file = bytearray()
     for file in files:
         dirname = file[1]
         _class = classes[dirname]
-        i = Image.open(os.path.join(*file))
-        i.thumbnail((25,25), Image.ANTIALIAS)
-        a = np.array(i)
-        rs = bytearray(a[:,:,0])
-        gs = bytearray(a[:,:,1])
-        bs = bytearray(a[:,:,2])
 
-        out_file += bytearray([_class]) + rs + gs + bs
+        out_file += image_to_labeled_bytearray(os.path.join(*file), _class)
 
     with open("%sBatches/batch_1.bin" % path, "wb") as f:
         f.write(out_file)
 
 convert("Training")
 convert("Validation")
-
-
 
 
 
