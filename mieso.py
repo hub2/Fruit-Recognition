4a78dbdb082a025901f26e7725548e26b6d10df0
Hubert Barc <hubert2222@gmail.com>
2018-04-09 11:56:07 +0200

    hyperparameters tuning

--- mieso.py
+++ mieso.py
@@ -4,7 +4,7 @@ import re
 import os
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('batch_size', 256,
+tf.app.flags.DEFINE_integer('batch_size', 150,
                             "Number of images to process in a batch")
 tf.app.flags.DEFINE_string('data_dir', "TrainingBatches",
                             "Path to the batch directory")
